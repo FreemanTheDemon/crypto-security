5356cada32aab206eedea3d6354de7f9bb81b239
Freeman Bird <freemanbird@gmail.com>
2021-12-02 14:42:31 -0700

    completed part 1

--- part-1/server/controllers/auth.js
+++ part-1/server/controllers/auth.js
@@ -1,4 +1,5 @@
 const users = []
+const bcrypt = require('bcryptjs');
 
 module.exports = {
     login: (req, res) => {
@@ -6,16 +7,37 @@ module.exports = {
       console.log(req.body)
       const { username, password } = req.body
       for (let i = 0; i < users.length; i++) {
-        if (users[i].username === username && users[i].password === password) {
-          res.status(200).send(users[i])
+        const passExists = bcrypt.compareSync(password, users[i].password);
+        if (users[i].username === username && passExists) {
+          let returnedUser = {...users[i]}
+          delete returnedUser.password;
+          console.log('User found', returnedUser);
+          res.status(200).send(returnedUser);
+          return;
         }
       }
       res.status(400).send("User not found.")
     },
     register: (req, res) => {
         console.log('Registering User')
-        console.log(req.body)
-        users.push(req.body)
-        res.status(200).send(req.body)
+        console.log(req.body);
+        
+        const {username, email, firstName, lastName, password} = req.body;
+        
+        let salt = bcrypt.genSaltSync(5);
+        
+        let passHash = bcrypt.hashSync(password, salt);
+        
+        let userObj = {
+          username,
+          email,
+          firstName,
+          lastName,
+          password: passHash
+        };
+        
+        console.log(userObj);
+        users.push(userObj)
+        res.status(200).send(userObj)
     }
 }
